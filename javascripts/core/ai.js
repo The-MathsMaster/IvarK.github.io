d8e1bda643d4f7846a32b1fb42a1496bccd818d8
dystopias-user <50160441+The-MathsMaster@users.noreply.github.com>
2020-05-12 23:05:40 +0800

    Update ai.js

--- javascripts/core/ai.js
+++ javascripts/core/ai.js
@@ -1,1 +1,11 @@
-
+function canBuyAiPt () {
+    return new Decimal("1e100").pow((player.ai.boughPoints/100)+1).gte(player.eternityPoints) && Math.pow(10, (player.ai.boughPoints/100)) >= player.timestudy.theorem;
+}
+function buyAiPt () {
+    if (canBuyAiPt()) {
+        player.eternityPoints = player.eternityPoints.sub(new Decimal("1e100").pow((player.ai.boughPoints/100)+1));
+        player.timestudy.theorom += -Math.pow(10, (player.ai.boughPoints/100));
+        player.ai.boughPoints += 100;
+        player.ai.aiPoints += 100;
+    }
+}
